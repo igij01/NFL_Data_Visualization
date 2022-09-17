a66d0af6f4d28fd59a310423e52d0583a84ee6e3
ericq <qian212@purdue.edu>
2022-09-17 13:32:33 -0400

    a

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 # Press ⇧F10 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import plotly
+import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
