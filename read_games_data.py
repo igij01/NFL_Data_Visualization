d04ac1aa0ece0d629285ae5e90e509f3bcc8dc2e
ericq <qian212@purdue.edu>
2022-09-17 17:55:31 -0400

    a

--- read_games_data.py
+++ read_games_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import read_from_csv
 
-class ReadGamesData(read_from_csv):
-    def __init__(self, path):
-        super.__init__(path)
-        self.path[["type", "game_id"]]
+# class ReadGamesData(read_from_csv):
+#     def __init__(self, path):
+#         super.__init__(path)
+#         self.path[["type", "game_id"]]
