09b1395291dc3b001f282acf8f506d7961825b4a
igij01 <lin1625@purdue.edu>
2022-09-17 23:28:15 -0400

    Fixed all kinds of bugs

--- web_app.py
+++ web_app.py
@@ -1,6 +1,4 @@
-import read_from_csv
 import plotly.io as pio
-import read_games_data
 
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
@@ -9,10 +7,13 @@ from dash.dependencies import Input, Output
 from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
 
 
-
-def conversion(val):
+def conversion_float(val):
     new_val = val.replace(',', '')
     return float(new_val)
+
+
+def conversion_percentile(val):
+    return int(val * 100)
 
 
 app = Dash(__name__)
@@ -24,7 +25,7 @@ app.layout = html.Div([
                      {"label": 'Rushing Leader Yards', "value": "Rushing_leader_by_year.csv"},
                      {"label": 'QB Passing Leader Yards', "value": "passing_leader_yards_by_year.csv"},
                      {"label": 'Passing yds/rushing yds', "value": "P/R Ratio"}],
-                 value='passing_leader_yards_by_year.csv'),
+                 value='Rushing_leader_by_year.csv'),
     dcc.Graph(id='nfl_map', figure={})
 ])
 
@@ -33,23 +34,28 @@ app.layout = html.Div([
 
 def update_graph(value):
     if value == 'P/R Ratio':
-        object1 = read_from_csv.ReadCsv(
-        "C:\\Users\\ericq\\PycharmProjects\\NFL_Data_Visualization\\games_data\\passing_leader_yards_by_year.csv")
-        object2 = read_from_csv.ReadCsv(
-            "C:\\Users\\ericq\\PycharmProjects\\NFL_Data_Visualization\\games_data\\Rushing_leader_by_year.csv")
-        object1.data.Yards = object1.data.Yards.apply(conversion)
-        object1 = object1.filter_column(["Year", "Yards"])
-        object2.data.Yards = object2.data.Yards.apply(conversion)
-        object2 = object2.filter_column(["Year", "Yards"])
-        object = object1
-        object.Yards = object1.Yards / object2.Yards
+        df1 = pd.read_csv(
+        # "C:\\Users\\ericq\\PycharmProjects\\NFL_Data_Visualization\\games_data\\passing_leader_yards_by_year.csv")
+            "games_data/passing_leader_yards_by_year.csv").drop_duplicates(subset="Year")
+        df2 = pd.read_csv(
+            # "C:\\Users\\ericq\\PycharmProjects\\NFL_Data_Visualization\\games_data\\Rushing_leader_by_year.csv")
+            "games_data/Rushing_leader_by_year.csv").drop_duplicates(subset="Year")
+        df1.Yards = df1.Yards.apply(conversion_float)
+        df2.Yards = df2.Yards.apply(conversion_float)
+        df1.index = df2.index
+        df = df1.copy()
+        df.Yards = df1["Yards"].div(df2["Yards"])
+        df.rename(columns={"Yards": "P yds/R yds"}, inplace=True)
+        # df.Yards = df.Yards.apply(conversion_percentile)
+        fig = px.scatter(df, x="Year", y="P yds/R yds")
     else:
-        object = read_from_csv.ReadCsv(
-        "C:\\Users\\ericq\\PycharmProjects\\NFL_Data_Visualization\\games_data\\" + value)
-        object = object.filter_column(["Year", "Yards"])
-        object.Yards = object.Yards.apply(conversion)
-    object = object.drop_duplicates(subset="Year").sort_values(by="Yards", ascending=False)
-    fig = px.scatter(object, x="Year", y="Yards")
+        df = pd.read_csv(
+        # "C:\\Users\\ericq\\PycharmProjects\\NFL_Data_Visualization\\games_data\\"
+            "games_data/"
+        + value)
+        df.Yards = df.Yards.apply(conversion_float)
+        df = df.drop_duplicates(subset="Year")
+        fig = px.scatter(df, x="Year", y="Yards")
     return fig
 #
 # fig = px.line(object.loc[object['QB Passing Leader Yards'] == ])
